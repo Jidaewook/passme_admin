cb8a926214967249e1eee1b717ca1eecc693ebda
Jidaewook <psatdoctor@naver.com>
2021-12-12 11:38:58 +0900

    Update mainPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import "./App.css"
+import { Footer, Navibar } from './components';
 
 const App = () => {
   return (
     <div className='App'>
-      <h1>
-        PASS ME
-      </h1>
+      <Navibar />
+      <Footer />
     </div>
+
   );
 };
 
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Footer from "./Footer";
+import Navibar from "./Navibar";
+
+export {Footer, Navibar};
